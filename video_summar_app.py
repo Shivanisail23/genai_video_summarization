6c8efd5c0cd806ce02fd58fea8e6b1a64dd696a6
Shivanisail23 <sailshivani2005@gmail.com>
2025-04-23 14:47:24 +0530

    Update video_summar_app.py

--- video_summar_app.py
+++ video_summar_app.py
@@ -87,7 +87,7 @@ def turn_into_story(summary):
 
 # Streamlit UI
 st.title("📺 BBQ- YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("PragyanAI_Transperent.png")
+st.image("NETWORK.png")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
